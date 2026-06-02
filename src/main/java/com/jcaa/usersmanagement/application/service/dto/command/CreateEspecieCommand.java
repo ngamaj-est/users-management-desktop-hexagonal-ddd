ea040ee7ff8285e086ba0f976f65c31e1a2d054b
ngamaj-est <nganaj@unicartagena.edu.co>
2026-06-01 22:50:10 -0500

    feat(service): create commands for especie

---
+++ src/main/java/com/jcaa/usersmanagement/application/service/dto/command/CreateEspecieCommand.java
@@ -0,0 +1,18 @@
+package com.jcaa.usersmanagement.application.service.dto.command;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateEspecieCommand(
+    @NotBlank(message = "id must not be blank") String id,
+    @NotBlank(message = "nombre must not be blank")
+        @Size(min = 2, message = "nombre must have at least 2 characters")
+        String nombre,
+    @NotBlank(message = "nombreCientifico must not be blank")
+        @Size(min = 3, message = "nombreCientifico must have at least 3 characters")
+        String nombreCientifico,
+    String descripcion,
+    String habitat) 
+    {
+
+    }
