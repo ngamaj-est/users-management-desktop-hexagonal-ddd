ad01e27262957661a56622f9903f628621359318
ngamaj-est <nganaj@unicartagena.edu.co>
2026-06-01 20:31:49 -0500

    feat(model): create modelo para especie

---
+++ src/main/java/com/jcaa/usersmanagement/domain/model/EspecieModel.java
@@ -0,0 +1,26 @@
+package com.jcaa.usersmanagement.domain.model;
+
+import com.jcaa.usersmanagement.domain.valueobject.EspecieId;
+import com.jcaa.usersmanagement.domain.valueobject.NombreCientifico;
+import com.jcaa.usersmanagement.domain.valueobject.NombreEspecie;
+
+import lombok.Value;
+
+@Value
+public class EspecieModel {
+
+    EspecieId id;
+    NombreEspecie nombre;
+    NombreCientifico nombreCientifico;
+    String descripcion;
+    String habitat;
+
+    public static EspecieModel create(
+            final EspecieId id,
+            final NombreEspecie nombre,
+            final NombreCientifico nombreCientifico,
+            final String descripcion,
+            final String habitat) {
+        return new EspecieModel(id, nombre, nombreCientifico, descripcion, habitat);
+    }
+}
